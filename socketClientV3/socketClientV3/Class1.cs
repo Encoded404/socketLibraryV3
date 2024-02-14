ab0dc925954dac3b778454bc0a01c9d4959f39ec
Enc0dedClass_scpc <aronshoey@gmail.com>
2024-02-14 11:18:09 +0100

    initial commit
    and a little variable stuff

---
+++ socketClientV3/socketClientV3/Class1.cs
@@ -0,0 +1,19 @@
+﻿namespace socketClientV3
+{
+    public class socketClient
+    {
+        int currentAutoId = 0;
+        Dictionary<int, object> data = new Dictionary<int, object>();
+
+        public void addSharedVariable(ref object variable, string id = "auto")
+        {
+            if(id= = "auto")
+            {
+                for(int i  = currentAutoId, pusher = 1; i < pusher;  i++)
+                {
+
+                }
+            }
+        }
+    }
+}
