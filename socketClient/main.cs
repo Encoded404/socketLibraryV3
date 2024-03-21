ae8e0e6785048dd393eedfd8d99bff6a87cf1c3b
Enc0dedClass <75113441+Enc0dedClass@users.noreply.github.com>
2024-03-21 16:37:16 +0100

    idk

---
+++ socketClient/main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+
+namespace socketClient
+{
+    public class socketClient
+    {
+
+        static void Main()
+        {
+            Console.WriteLine("hello, starting");
+        }
+    }
+
+    class deepSocketClient
+    {
+        Socket socketSend(Socket socket, byte[] data)
+        {
+            
+        }
+    }
+}
